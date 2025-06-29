0d45a075c3b52bebae662aedea2a909a76e4534b
Dave Travis <163664552+davietraviso@users.noreply.github.com>
2025-06-29 17:31:40 +0700

    update index.js

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: true
+      rejectUnauthorized: false
     }
   }
 });
